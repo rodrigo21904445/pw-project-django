f59aec5b6480b8ac6ab27f3fff1f1a3ddcb7229f
rodrigo21904445 <a21904445@alunos.ulht.pt>
2021-06-04 21:00:06 +0100

    sheeeeesh

--- soundify/urls.py
+++ soundify/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.shortcuts import render
 from . import views
 from django.urls import path
@@ -5,5 +6,10 @@ from django.urls import path
 app_name = "soundify"
 
 urlpatterns = [
-    path('', views.index_page_view, name='index')
+    path('', views.index_page_view, name='index'),
+    path('index.html', views.index_page_view, name="index"),
+    path('search.html', views.search_page_view, name='search'), 
+    path('login.html', views.login_page_view, name="login"),
+    path('register.html', views.register_page_view, name="register"),
+    path('feedback.html', views.feedback_page_view, name="feedback"),
 ]
--- soundify/views.py
+++ soundify/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index_page_view(request):
     return render(request, "soundify/index.html")
+
+def search_page_view(request):
+    return render(request, "soundify/search.html")
+
+def login_page_view(request):
+    return render(request, 'soundify/login.html')
+
+def register_page_view(request):
+    return render(request, 'soundify/register.html')
+
+def feedback_page_view(request):
+    return render(request, 'soundify/feedback.html')
+
