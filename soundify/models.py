5ea5073865093828873b5535d38cce30ee8d9e5c
rodrigo21904445 <a21904445@alunos.ulht.pt>
2021-06-08 20:26:23 +0100

    sheeeeesh

--- soundify/models.py
+++ soundify/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    user = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    duration = models.IntegerField()
+    genre = models.CharField(max_length=64)
