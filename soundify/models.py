70d85be65568008426a80f147f3d79c15dc78999
rodrigo21904445 <a21904445@alunos.ulht.pt>
2021-06-10 17:11:04 +0100

    sheeesh

--- soundify/models.py
+++ soundify/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=64)
+    username = models.CharField(max_length=64, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
     password = models.CharField(max_length=32)
     email = models.EmailField(max_length=32)
@@ -20,5 +21,9 @@ class Music(models.Model):
     def __str__(self):
         return f"{self.user} uploaded {self.name}: {self.duration} ({self.genre})"
 
+class Playlist(self):
+    name = models.ManyToManyField(Music, blank=True)
+    user = ForeignKey(User)
+    genre = models.CharField(max_length=64)
     
 
