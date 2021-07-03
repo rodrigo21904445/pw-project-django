6695dc57355ad734e58b4005dfc7301d0850a1b9
rodrigo21904445 <a21904445@alunos.ulht.pt>
2021-07-03 03:17:52 +0100

    sheesh

--- soundify/static/soundify/js/index.js
+++ soundify/static/soundify/js/index.js
@@ -1,7 +1,30 @@
-const navButton = document.querySelectorAll('nav > ul > li > a');
+function showPage(page) {
+    console.log(`${page}`);
+    fetch(`${page}.html`)
+    .then(response => response.text())
+    .then(text => document.querySelector('#main-content').innerHTML = text);
+    console.log("hello there2");
+}
+
+document.querySelectorAll('nav').forEach(a => {
+    console.log("hello there1");
+    a.onclick = () => {
+        showPage(this.dataset);
+        console.log("hello there3");
+    };
+});
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    navButton.onclick = () => {
-        navButton.style.color = green;
-    }
+
+    
+
+    document.querySelectorAll('nav').forEach(a => {
+        console.log("hello there1");
+        a.onclick = () => {
+            showPage(this.dataset);
+            console.log("hello there3");
+        };
+    });
+    
 });
